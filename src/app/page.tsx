05914dd5e666ef01b9c0f1ad51ad731b8c357d2a
magda.wozna <magdalena.wozna@absolvent.pl>
2024-05-17 08:17:40 +0200

    Get data from api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,13 @@
-import {type Bunker} from "@/types"
-
-//TODO: add env
-async function getBunkers() {
-    const res = await fetch('http://localhost:3000/api/get-bunkers')
-
-    if (!res.ok) {
-        throw new Error('Failed to fetch bunkers')
-    }
-
-    return res.json()
-}
-
 export default async function Home() {
-    const { bunkers } = await getBunkers()
+    const res = await fetch("https://api.dane.gov.pl/1.4/resources/54637,schrony-zlokalizowane-w-miescie-krosno/data");
+    const { data: bunkers } = await res.json()
 
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
      <p className="mb-20">Hello from Krosno Bunkers!</p>
         <ul>
-            {bunkers.map(({id, longitude, latitude, capacity, address}: Bunker) => (
-                <li key={id}>{longitude}, {latitude}, {capacity}, {address}</li>
+            {bunkers.map((bunker => bunker.attributes)).map(attr => (
+                <li key={`${attr.col1.val}${attr.col2.val}`}>{attr.col5.val}, {attr.col4.val}, {attr.col1.val}, {attr.col2.val}</li>
             ))}
         </ul>
     </main>
