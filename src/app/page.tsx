3d6dae47c42116d8637be71919870962ac447047
magda.wozna <magdalena.wozna@absolvent.pl>
2024-05-24 15:21:58 +0200

    Get fetching bunkers back

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,27 @@
 import { type Bunker } from '@/types'
 
-// async function getBunkers() {
-//   const res = await fetch(`${process.env.NEXT_PUBLIC_APP_URL}/api/bunkers`)
-//
-//   if (!res.ok) {
-//     throw new Error('Failed to fetch bunkers')
-//   }
-//
-//   return res.json()
-// }
+async function getBunkers() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_APP_URL}/api/bunkers`)
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch bunkers')
+  }
+
+  return res.json()
+}
 
 export default async function Home() {
-  // const { data: bunkers } = await getBunkers()
+  const { data: bunkers } = await getBunkers()
 
   return (
     <main className="w-full text-white bg-home-hero bg-cover bg-center bg-no-repeat justify-center flex min-h-screen flex-col items-center p-24">
       <p className="mb-20 text-5xl">Hello from Krosno Bunkers!</p>
       <ul>
-        {/*{bunkers.map(({ id, longitude, latitude, capacity, address }: Bunker) => (*/}
-        {/*  <li key={id}>*/}
-        {/*    {longitude}, {latitude}, {capacity}, {address}*/}
-        {/*  </li>*/}
-        {/*))}*/}
+        {bunkers.map(({ id, longitude, latitude, capacity, address }: Bunker) => (
+          <li key={id}>
+            {longitude}, {latitude}, {capacity}, {address}
+          </li>
+        ))}
       </ul>
     </main>
   )
