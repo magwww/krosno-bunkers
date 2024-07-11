2c4185cf38c7b3c0925d264c36bb3e42fba4b803
Magda <wozna.magda@gmail.com>
2024-07-11 20:09:17 +0200

    Style preview page

--- src/app/components/preview/preview-content.tsx
+++ src/app/components/preview/preview-content.tsx
@@ -17,15 +17,22 @@ export default function PreviewContent({ bunker }: { bunker: Stripe.Price }) {
 
   return (
     <div className="flex flex-col w-full h-screen justify-center items-center">
-      <p className="text-lg mb-10">Wybrany bunkier: {bunker.metadata.address}</p>
+      <p className="text-lg mb-16 flex flex-col items-center text-center">
+        Od zostania szczęśliwym posiadaczem miejsca w Twoim wybranym bunkrze:{' '}
+        <span className="font-bold my-4 text-xl">{bunker.metadata.address}</span> dzieli Cie tylko jedno kliknięcie!
+      </p>
       <form>
         <button
           type="submit"
           role="link"
-          className="border border-white px-3 py-2 rounded"
+          className="group relative inline-block overflow-hidden transition-all duration-500 hover:bg-black bg-white/10 rounded px-12 py-3 text-sm font-medium text-white focus:outline-none active:text-white"
           onClick={handleSubscription}
         >
-          Checkout
+          <span className="ease absolute left-0 top-0 h-0 w-0 border-t-2 border-white transition-all duration-300 group-hover:w-full"></span>
+          <span className="ease absolute right-0 top-0 h-0 w-0 border-r-2 border-white transition-all duration-300 group-hover:h-full"></span>
+          <span className="ease absolute bottom-0 right-0 h-0 w-0 border-b-2 border-white transition-all duration-300 group-hover:w-full"></span>
+          <span className="ease absolute bottom-0 left-0 h-0 w-0 border-l-2 border-white transition-all duration-300 group-hover:h-full"></span>
+          Przejdź do płatności
         </button>
       </form>
     </div>
