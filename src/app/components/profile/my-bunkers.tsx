33980789ccfbe8df38a04cc5c1efe56f80191fc6
Magda <wozna.magda@gmail.com>
2024-07-13 15:51:09 +0200

    Fix build

--- src/app/components/profile/my-bunkers.tsx
+++ src/app/components/profile/my-bunkers.tsx
@@ -1,7 +1,7 @@
 export default function MyBunkers() {
   return (
     <div className="flex flex-col h-screen justify-center dark:text-white items-center">
-      You'll see your bunkers here
+      You&apos;ll see your bunkers here
     </div>
   )
 }
