7faf8c271d797e014b07e231c441058237203874
Magda <wozna.magda@gmail.com>
2024-07-22 13:15:41 +0200

    Change creating checkout sessions to payment intent

--- src/app/api/create-payment-intent/route.ts
+++ src/app/api/create-payment-intent/route.ts
@@ -2,23 +2,49 @@ import { NextRequest, NextResponse } from 'next/server'
 import { stripe } from '@/lib/stripe'
 import Stripe from 'stripe'
 import { type Bunker } from '@/types'
+import { db } from '@/lib/db'
+import { auth } from '@clerk/nextjs/server'
 
+//TODO: enable multiple products purchase
 const calculateOrderAmount = (items: Bunker[]) => {
-  const amount = items.reduce((accumulator, item) => accumulator + item.price, 0)
-  return amount
+  return items.reduce((accumulator, item) => accumulator + item.price, 0)
 }
 
 export async function POST(req: NextRequest, res: NextResponse) {
   const { items } = await req.json()
+  const { userId } = auth()
+
+  if (!userId || !items.length) {
+    return NextResponse.json({ error: 'Invalid request - no user or products selected' }, { status: 400 })
+  }
 
   try {
     const paymentIntent = await stripe.paymentIntents.create({
       amount: calculateOrderAmount(items),
       currency: 'PLN',
+      // metadata: {
+      //   order_id: 'test',
+      //   bunkerId: items[0].id,
+      //   userId: 'test',
+      // },
+    })
+
+    //TODO: enable multiple products purchase
+    //TODO: get dynamic userId after syncing clerk with db
+
+    const order = await db.order.create({
+      data: {
+        price: items[0].price,
+        userId: '2',
+        bunkers: {
+          connect: [items[0].id],
+        },
+      },
     })
 
     return NextResponse.json({
       clientSecret: paymentIntent.client_secret,
+      order,
     })
   } catch (err: unknown) {
     if (err instanceof Stripe.errors.StripeError) {
