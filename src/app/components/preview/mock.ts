23f14b35c18aa22669d7557594df1f82d701ac39
Dawid Janiga <dawidjaniga@gmail.com>
2024-08-09 15:20:56 +0200

    test: add create payment intent mock

---
+++ src/app/components/preview/mock.ts
@@ -0,0 +1,19 @@
+import { http, HttpResponse } from 'msw'
+
+export const successPaymentIntent =
+    http.post('/api/create-payment-intent', () => {
+        return HttpResponse.json(
+            {
+                "clientSecret": 'qwerty-123',
+                "order": {
+                    "id": "132a7c1a-c0d7-4de4-a5a1-fe5af966abe9",
+                    "createdAt": "2024-08-09T09:56:10.597Z",
+                    "updatedAt": "2024-08-09T09:56:10.597Z",
+                    "price": 200,
+                    "userId": "b13b311c-a0dd-4704-8882-37bcc6518f88",
+                    "bunkerId": "6d9caa03-1048-4fec-b906-7f8bef4d3c23",
+                    "paid": false
+                }
+            }
+        )
+    })
