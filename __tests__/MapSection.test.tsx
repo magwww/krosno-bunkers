32137096df0cb37646932f82a7f6b5691e4791bf
Magda <wozna.magda@gmail.com>
2024-08-23 13:47:56 +0200

    Add bunkers list test

--- __tests__/MapSection.test.tsx
+++ __tests__/MapSection.test.tsx
@@ -1,4 +1,5 @@
 import MapSection from '@/app/components/home/map-section'
+import { bunkers } from '@/mocks/bunkers'
 
 import { render, screen } from '@testing-library/react'
 
@@ -18,90 +19,6 @@ jest.mock('next/navigation', () => {
     }),
   }
 })
-
-//TODO: mock data fetching
-const bunkers = [
-  {
-    id: '1',
-    longitude: 21.7680694093419,
-    latitude: 49.690988867662234,
-    capacity: 1,
-    address: 'Staszica 12',
-    price: 200,
-  },
-  {
-    id: '2',
-    longitude: 21.7680694093419,
-    latitude: 49.690988867662234,
-    capacity: 1,
-    address: 'Kolejowa 8',
-    price: 200,
-  },
-  {
-    id: '3',
-    longitude: 21.7680694093419,
-    latitude: 49.690988867662234,
-    capacity: 1,
-    address: 'Kolejowa 10',
-    price: 200,
-  },
-  {
-    id: '4',
-    longitude: 21.7680694093419,
-    latitude: 49.690988867662234,
-    capacity: 1,
-    address: 'Kolejowa 14',
-    price: 200,
-  },
-  {
-    id: '5',
-    longitude: 21.7680694093419,
-    latitude: 49.690988867662234,
-    capacity: 1,
-    address: 'Naftowa 9',
-    price: 200,
-  },
-  {
-    id: '6',
-    longitude: 21.7680694093419,
-    latitude: 49.690988867662234,
-    capacity: 1,
-    address: 'Naftowa 17/3',
-    price: 200,
-  },
-  {
-    id: '7',
-    longitude: 21.7680694093419,
-    latitude: 49.690988867662234,
-    capacity: 1,
-    address: 'Naftowa 17/5',
-    price: 200,
-  },
-  {
-    id: '8',
-    longitude: 21.7680694093419,
-    latitude: 49.690988867662234,
-    capacity: 1,
-    address: 'Wolności 3',
-    price: 200,
-  },
-  {
-    id: '9',
-    longitude: 21.7680694093419,
-    latitude: 49.690988867662234,
-    capacity: 1,
-    address: 'Kletówki 27a',
-    price: 200,
-  },
-  {
-    id: '10',
-    longitude: 21.7680694093419,
-    latitude: 49.690988867662234,
-    capacity: 1,
-    address: 'Naftowa 8',
-    price: 200,
-  },
-]
 
 test('given correct API KEY is passed from ENV, it renders google map', () => {
   process.env.NEXT_PUBLIC_MAPS_API_KEY = 'correct api key'
--- __tests__/MyBunkers.test.tsx
+++ __tests__/MyBunkers.test.tsx
@@ -1,9 +1,25 @@
 import MyBunkers from '@/app/components/profile/my-bunkers'
-import { render, screen } from '@testing-library/react'
+import { render, screen, within } from '@testing-library/react'
+import { bunkers } from '@/mocks/bunkers'
 
-it('renders no bunker spots purchased message when no bunkers are passed', async () => {
+it('renders no bunker spots purchased message when no bunkers are passed and data is not loading', async () => {
   render(<MyBunkers isLoading={false} bunkers={[]} />)
 
   const message = await screen.findByText(/You don't own any bunker spots yet/)
   expect(message).toBeInTheDocument()
+})
+
+it('renders list of 10 bunkers when 10 bunkers are passed and data is not loading', async () => {
+  render(<MyBunkers isLoading={false} bunkers={bunkers} />)
+
+  const firstBunker = await screen.findByText(/Staszica 12/)
+  const secondBunker = await screen.findByText(/Kolejowa 8/)
+
+  expect(firstBunker).toBeInTheDocument()
+  expect(secondBunker).toBeInTheDocument()
+
+  const list = screen.getByRole('list')
+  const listItems = within(list).getAllByRole('listitem')
+
+  expect(listItems).toHaveLength(10)
 })
