bf285cd0df57407e514dbdf14f6fbd9057737a47
Magda <wozna.magda@gmail.com>
2024-08-30 15:14:45 +0200

    Add location checks in cypress test

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -11,16 +11,18 @@ describe('Home page', () => {
 
 describe('Bunkers page', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000')
+    cy.visit('http://localhost:3000/')
     cy.contains('Select your bunker').click()
   })
 
   it('displays google map with 10 markers', () => {
+    cy.location('pathname').should('eq', '/bunkers')
     cy.get('[data-testid=google-map]').should('exist')
     cy.get('[class*="marker-view"]').should('have.length', 10)
   })
 
   it('displays info window after marker click', () => {
+    cy.location('pathname').should('eq', '/bunkers')
     cy.get('[class*="marker-view"]').first().click({ force: true })
     cy.get('[data-testid=google-map-info-window').should('exist')
     cy.contains('Buy spot in this bunker').should('exist')
@@ -36,6 +38,7 @@ describe('Bunker preview page', () => {
   })
 
   it('redirects to sign in page', () => {
+    cy.location('pathname').should('include', 'sign-in')
     cy.contains('You need to log in to buy spot in this bunker').should('exist')
   })
 })
