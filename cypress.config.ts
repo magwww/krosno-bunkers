bd804838d3c5b677cd5dcf3d775e4b20f86270e5
Magda <wozna.magda@gmail.com>
2024-09-16 14:43:34 +0200

    Add cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,13 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress'
+import { clerkSetup } from '@clerk/testing/cypress'
 
 export default defineConfig({
+  chromeWebSecurity: false,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      return clerkSetup({ config })
     },
+    baseUrl: 'http://localhost:3000', // your app's URL
   },
-});
+})
