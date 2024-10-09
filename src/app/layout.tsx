da2edc3e7a051ccd69b6eccec44b873a4922cd78
Magda <wozna.magda@gmail.com>
2024-10-09 14:52:31 +0200

    Add react query provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,8 @@ import { ThemeProvider } from '@/app/components/common/theme-provider'
 import Navigation from '@/app/components/common/navigation'
 import { ClerkProvider } from '@clerk/nextjs'
 import { Toaster } from 'react-hot-toast'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import ReactQueryProvider from './react-query-provider'
 
 const anek = Anek_Latin({
   weight: ['400', '500', '700'],
@@ -27,22 +29,25 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning className={anek.className}>
       <head />
       <body>
-        <ClerkProvider>
-          <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-            <Toaster
-              position="bottom-right"
-              toastOptions={{
-                duration: 5000,
-                style: {
-                  background: 'black',
-                  color: '#fff',
-                },
-              }}
-            />
-            <Navigation />
-            {children}
-          </ThemeProvider>
-        </ClerkProvider>
+        <ReactQueryProvider>
+          <ReactQueryDevtools />
+          <ClerkProvider>
+            <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+              <Toaster
+                position="bottom-right"
+                toastOptions={{
+                  duration: 5000,
+                  style: {
+                    background: 'black',
+                    color: '#fff',
+                  },
+                }}
+              />
+              <Navigation />
+              {children}
+            </ThemeProvider>
+          </ClerkProvider>
+        </ReactQueryProvider>
       </body>
     </html>
   )
