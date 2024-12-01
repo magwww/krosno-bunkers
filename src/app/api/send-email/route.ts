86282701a409a3260189dd68d828d9c37abbe3c2
Magda <wozna.magda@gmail.com>
2024-12-01 11:30:40 +0100

    Add sending email api route

---
+++ src/app/api/send-email/route.ts
@@ -0,0 +1,24 @@
+import BunkerPurchaseConfirmation from '@/emails/bunker-purchase-confirmation'
+import { Resend } from 'resend'
+
+const resend = new Resend(process.env.RESEND_API_KEY)
+
+export async function POST(req: Request, res: Response) {
+  try {
+    const { email, firstName } = await req.json()
+    const { data, error } = await resend.emails.send({
+      from: 'Krosno Bunkers <hello@krosno-bunkers.pl>',
+      to: email,
+      subject: 'Bunker order confirmation',
+      react: BunkerPurchaseConfirmation({ firstName }),
+    })
+
+    if (error) {
+      return Response.json({ error }, { status: 500 })
+    }
+
+    return Response.json({ message: 'Email sent successfully', data })
+  } catch (error) {
+    return Response.json({ error }, { status: 500 })
+  }
+}
