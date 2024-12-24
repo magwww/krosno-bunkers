749e6010f91455ec2892723d8d5dffabdb204293
Magda <wozna.magda@gmail.com>
2024-12-25 00:09:50 +0100

    Revert "Enable cors for krosno-bunkers.pl"
    This reverts commit c8c091acee97d39bafb03ae1d5591bb2a2755324.

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 import {withSentryConfig} from '@sentry/nextjs';
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
+const nextConfig = {images: {
     remotePatterns: [
       {
         protocol: 'https',
@@ -9,19 +8,6 @@ const nextConfig = {
         port: '',
       },
     ],
-  },
-  async headers() {
-    return [
-      {
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "https://krosno-bunkers.pl" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
-      }
-    ]
   }
 };
 
