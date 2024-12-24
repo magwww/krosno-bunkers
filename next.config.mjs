c8c091acee97d39bafb03ae1d5591bb2a2755324
Magda <wozna.magda@gmail.com>
2024-12-24 19:23:51 +0100

    Enable cors for krosno-bunkers.pl

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 import {withSentryConfig} from '@sentry/nextjs';
 /** @type {import('next').NextConfig} */
-const nextConfig = {images: {
+const nextConfig = {
+  images: {
     remotePatterns: [
       {
         protocol: 'https',
@@ -8,6 +9,19 @@ const nextConfig = {images: {
         port: '',
       },
     ],
+  },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "https://krosno-bunkers.pl" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
   }
 };
 
