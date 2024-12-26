3c1bd41afb1c5ecbffe4f7fa28db261a9fbde626
Magda <wozna.magda@gmail.com>
2024-12-26 11:33:32 +0100

    Update main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import { ButtonLinkBorderedAnimated } from '@/app/components/common/button-bordered-animated'
 import { useEffect, useState } from 'react'
-import { cn } from '@/lib/utils'
+import { motion } from 'motion/react'
 
 export default function Home() {
   const [isMounted, setIsMounted] = useState<boolean>(false)
@@ -13,28 +13,25 @@ export default function Home() {
 
   return (
     <main className="before:block relative z-10 before:z-[-5] before:absolute before:inset-0 before:content-[''] bg-home-hero before:bg-gradient-to-t before:from-black before:to-transparent bg-cover bg-no-repeat bg-center before:opacity-70 w-full min-h-[calc(100vh-64px)]">
-      <div
-        className={cn(
-          'w-full p-10 lg:p-24 justify-center flex flex-col items-center min-h-[calc(100vh-64px)] transition-blur duration-1000',
-          !isMounted && 'blur-sm',
-        )}
-      >
-        <h1
-          className={cn(
-            'text-white mb-16 text-5xl lg:text-6xl text-center transition-text duration-1000 p-2 rounded-md',
-            !isMounted && 'text-4xl lg:text-4xl',
-          )}
+      <div className="flex flex-col justify-center items-center p-10 lg:p-24 w-full min-h-[calc(100vh-64px)]">
+        <motion.div
+          initial={{ y: 10, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          exit={{ y: -10, opacity: 0 }}
+          transition={{ duration: 0.5 }}
           data-testid="home-header"
         >
-          Welcome to Krosno Bunkers!
-        </h1>{' '}
-        <ButtonLinkBorderedAnimated
-          href="/bunkers"
-          className="bg-black/10 text-white lg:text-md transition-all duration-700"
-          data-testid="home-button"
-        >
-          Select your bunker
-        </ButtonLinkBorderedAnimated>
+          <h1 className="mb-16 p-2 rounded-md text-5xl text-center text-white/80 lg:text-6xl">
+            Welcome to Krosno Bunkers
+          </h1>
+          <ButtonLinkBorderedAnimated
+            href="/bunkers"
+            className="justify-self-center bg-black/10 text-white/80 lg:text-md transition-all duration-700"
+            data-testid="home-button"
+          >
+            Select your bunker
+          </ButtonLinkBorderedAnimated>
+        </motion.div>
       </div>
     </main>
   )
