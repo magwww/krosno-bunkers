8c6e9e0f8367a49a19adec1322e3cc43e933b952
Dawid Janiga <dawidjaniga@gmail.com>
2025-02-27 09:37:52 +0100

    feat(user-profile): use CreateUser Server Action

--- src/user/create-user.ts
+++ src/user/create-user.ts
@@ -4,8 +4,8 @@ import { CreateUserResponse, UserService } from './UserService'
 import { ClerkAuthService } from './ClerkAuthService'
 import { PrismaUserRepository } from './PrismaUserRepository'
 
-export async function createUser(): Promise<CreateUserResponse> {
-  const userService = new UserService(new ClerkAuthService(), new PrismaUserRepository())
+const userService = new UserService(new ClerkAuthService(), new PrismaUserRepository())
 
+export async function createUser(): Promise<CreateUserResponse> {
   return userService.createUser()
 }
